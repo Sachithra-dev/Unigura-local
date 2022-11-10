a6a944c1336b790ed5f7993aa6902a724308dc6d
MadushariniRanasinghe12 <87421056+MadushariniRanasinghe12@users.noreply.github.com>
2022-11-10 23:49:32 +0530

    Update tutorregistration.php

--- tutorregistration.php
+++ tutorregistration.php
@@ -29,18 +29,20 @@
 
 <body>
     <div class="container">
-        <h1>User Registration System</h1>
-        <h2><a href="loggedInPage.php">Home Page</a></h2>
-        <form action = "" method = "post">
-            <div class="error"><?php echo $error; ?></div>
-            <input type="text" name = "name" placeholder="user name"><br><br>
-            <input type="email" name = "email" placeholder="email"><br><br>
-            <input type="password" name = "password" placeholder="password"><br><br>
-            <input type="password" name = "repeatpassword" placeholder="Repeat Password"><br><br>
-            <label for="checkbox">Keep me logged in</label>
-            <input type="checkbox" name = "stayLoggedIn" value= "1"><br><br>
-            <input type="submit" name = "signup" value="Sign Up"><br><br>
-            <p>Have account already?<a href='login.php'>login</a></p>
+        <h1>Tutor Details</h1>
+            <form action = "" method = "post">
+                <div class="error"><?php echo $error; ?></div>
+                <input type="text" name = "FirstName" placeholder="FirstName"><br><br>
+                <input type="text" name = "LastName" placeholder="LastName"><br><br>
+                <input type="number" name = "telephone" placeholder="telephone"><br><br>
+                <input type="number" name = "homenumber" placeholder="homenumber"><br><br>
+                <input type="text" name = "street" placeholder="street"><br><br>
+                <input type="text" name = "city" placeholder="city"><br><br>
+                <input type="text" name = "subjects" placeholder="subjects"><br><br>
+                <input type="text" name = "modes" placeholder="modes"><br><br>
+                <input type="text" name = "medium" placeholder="medium"><br><br>
+                
+                <input type="submit" name = "next" value="Next"><br><br>
         </form>
     </div>
 </body>
